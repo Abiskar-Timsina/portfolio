2abe21f09004d68bdb0d108ac1275a98211310ad
Abiskar-Timsina <abiskartimsina4@gmail.com>
2021-11-19 21:14:59 +0545

    added refs to links

--- js/script.js
+++ js/script.js
@@ -1,54 +1,72 @@
-
-
 /* Nav Bar Animation*/
-let nav= document.getElementsByClassName("nav")[0]
-let drop = document.getElementsByClassName("dropdown image")[0]
+	{
+	let nav= document.getElementsByClassName("nav")[0]
+	let drop = document.getElementsByClassName("dropdown image")[0]
 
-let animation_for_nav = "transform 250ms ease-in";
-let translate_upwards = "translateY(-100px)";
-let translate_downwards = "translateY(-10px)"
+	let animation_for_nav = "transform 250ms ease-in";
+	let translate_upwards = "translateY(-100px)";
+	let translate_downwards = "translateY(-10px)"
 
-nav.style.transition= animation_for_nav;
-nav.style.transform= translate_downwards;
-
-setTimeout(() =>{
-nav.style.transition= animation_for_nav;
-nav.style.transform= translate_upwards;
-},1500)
-
-nav.addEventListener("mouseover",e => {
 	nav.style.transition= animation_for_nav;
 	nav.style.transform= translate_downwards;
-	drop.style.transform="rotate(180deg)"
-	e.stopPropagation();
-},{capture:true})
 
-nav.addEventListener("mouseout",e => {
+	setTimeout(() =>{
 	nav.style.transition= animation_for_nav;
-	nav.style.transform= translate_upwards; 
-	drop.style.transform="rotate(0deg)"
-	e.stopPropagation();
-},{capture:true})
+	nav.style.transform= translate_upwards;
+	},1500)
+
+	nav.addEventListener("mouseover",e => {
+		nav.style.transition= animation_for_nav;
+		nav.style.transform= translate_downwards;
+		drop.style.transform="rotate(180deg)"
+		e.stopPropagation();
+	},{capture:true})
+
+	nav.addEventListener("mouseout",e => {
+		nav.style.transition= animation_for_nav;
+		nav.style.transform= translate_upwards; 
+		drop.style.transform="rotate(0deg)"
+		e.stopPropagation();
+	},{capture:true})
+}
 /* Nav Bar animation end*/
 
-const pic = document.getElementsByClassName("landing")[0]
 
-let t = new TimelineMax();
+/* Landing page loading animation */
+{
+	// Picture animation
+	const pic = document.getElementsByClassName("landing")[0]
 
-t.fromTo(pic,1,{height:"0%"},{height:"120%"})
-t.fromTo(pic,1,{height:"120%"},{height:"80%"})
+	let t = new TimelineMax();
 
-setTimeout(()=> {console.log(str)})
+	t.fromTo(pic,1,{height:"0%"},{height:"120%"})
+	t.fromTo(pic,1,{height:"120%"},{height:"80%"})
 
-/*Type writer animation*/
-let name = "Hi, I'm Abiskar Timsina"
-name = name.split(" ")
-let str = new String();
+	// Socials animation
+	let socials = document.getElementsByClassName("socials")[0];
+	t.fromTo(socials,1,{height:"120%"},{height:"80%"})
 
-name.forEach(word =>{
-	str += word + " ";
-	setTimeout(()=> {	
-		document.getElementsByClassName("name")[0].innerHTML = str;
-		console.log(str)
-	},250)
-});
+}
+/* Landing page loading animation */
+
+/* Socials animation and linking */
+{
+	let fb = document.getElementsByClassName("fb")[0];
+	let ln = document.getElementsByClassName("linkedin")[0];
+	let gh = document.getElementsByClassName("github")[0];
+
+	fb.addEventListener("click",e=>
+	{
+		window.open("https://www.facebook.com/abiskar.timsina/")
+	});
+
+	ln.addEventListener("click",e=>{
+		window.open("https://np.linkedin.com/in/abiskar-timsina-ba646a21b")
+	});
+
+	gh.addEventListener("click",e=>{
+		window.open("https://github.com/Abiskar-timsina")
+	});
+}
+
+/* Socials animations and linking end*/
