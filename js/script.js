f1eac1bb56dd8210bc680f3ee89797ffdef6d37d
Abiskar-Timsina <abiskartimsina4@gmail.com>
2021-11-19 20:00:51 +0545

    cleanes the js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,29 @@
 /* Nav Bar Animation*/
 let nav= document.getElementsByClassName("nav")[0]
 let drop = document.getElementsByClassName("dropdown image")[0]
-console.log(drop)
 
-nav.style.transition="transform 250ms ease-in";
-nav.style.transform="translateY(-10px)";
+let animation_for_nav = "transform 250ms ease-in";
+let translate_upwards = "translateY(-100px)";
+let translate_downwards = "translateY(-10px)"
+
+nav.style.transition= animation_for_nav;
+nav.style.transform= translate_downwards;
 
 setTimeout(() =>{
-nav.style.transition="transform 250ms ease-in";
-nav.style.transform="translateY(-100px)";
+nav.style.transition= animation_for_nav;
+nav.style.transform= translate_upwards;
 },1500)
 
 nav.addEventListener("mouseover",e => {
-	nav.style.transition="transform 250ms ease-in";
-	nav.style.transform="translateY(-10px)";
+	nav.style.transition= animation_for_nav;
+	nav.style.transform= translate_downwards;
 	drop.style.transform="rotate(180deg)"
 	e.stopPropagation();
 },{capture:true})
 
 nav.addEventListener("mouseout",e => {
-	nav.style.transition="transform 250ms ease-in";
-	nav.style.transform="translateY(-100px)"; 
+	nav.style.transition= animation_for_nav;
+	nav.style.transform= translate_upwards; 
 	drop.style.transform="rotate(0deg)"
 	e.stopPropagation();
 },{capture:true})
